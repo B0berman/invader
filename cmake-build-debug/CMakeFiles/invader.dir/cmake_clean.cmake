221093ee13c32a2dd865e0922b0a757c118be629
Hugo <hugo.walbecq@epitech.eu>
2018-07-22 00:25:30 +0200

    set project

---
+++ cmake-build-debug/CMakeFiles/invader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/invader.dir/main.cpp.o"
+  "invader.pdb"
+  "invader"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/invader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
